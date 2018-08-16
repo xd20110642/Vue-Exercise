93ea6820bd11fe67ebc6c736dfbf7703e8027824
xd20110642 <790057064@qq.com>
2018-08-16 16:07:41 +0800

    修改打包编译错误

--- src/state/strote.js
+++ src/state/strote.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 Vue.use(Vuex)
-let car=JSON.parse(localStorage.getItem('goods')||'');
+let car=JSON.parse(localStorage.getItem('goods')||'[]');
 var store=new Vuex.Store({
     state:{
         //将购物车中的商品的数据用一个数组存储起来,存储一些商品的对象 {id:商品id,count:购买数量,price:商品的单价,selected:false}
